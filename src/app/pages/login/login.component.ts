643a6094d3fa54cd480bc6bc9bf4f6fe6f37004a
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-06 17:45:18 +0100

    form is ready and we are able now to get form values and submit the form

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
 import { BG_IMG_URL, LOGO_URL } from '../../constants/config';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
   logoUrl = LOGO_URL;
   bgUrl = BG_IMG_URL;
+
+  email!: string;
+  password!: string;
+
+  onSubmit() {
+    // validate email and password
+    if (!this.email || this.password) {
+      alert('Provide email or password');
+      return;
+    }
+    // if email and password is correct lets login the user
+  }
 }
