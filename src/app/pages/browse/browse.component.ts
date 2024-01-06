17983b6145697259004ba6db4110ae14dd4f4377
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-06 19:09:34 +0100

    added angular guard

--- src/app/pages/browse/browse.component.ts
+++ src/app/pages/browse/browse.component.ts
@@ -1,6 +1,4 @@
-import { Component, inject } from '@angular/core';
-import { LoginService } from '../../services/login.service';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -10,14 +8,4 @@ import { CommonModule } from '@angular/common';
   templateUrl: './browse.component.html',
   styleUrl: './browse.component.css',
 })
-export class BrowseComponent {
-  loginService = inject(LoginService);
-  router = inject(Router);
-
-  ngOnInit(): void {
-    // if user is already logged in then redirect to browse page
-    if (!this.loginService.isLoggedIn) {
-      this.router.navigateByUrl('/login');
-    }
-  }
-}
+export class BrowseComponent {}
