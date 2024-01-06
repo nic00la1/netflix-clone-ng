0503998dee9808d30964f06495523d82596db71d
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-06 18:27:25 +0100

    Wow our login-page is ready

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { BG_IMG_URL, LOGO_URL } from '../../constants/config';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { LoginService } from '../../services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -16,13 +18,18 @@ export class LoginComponent {
 
   email!: string;
   password!: string;
+  loginService = inject(LoginService);
+  router = inject(Router);
 
   onSubmit() {
     // validate email and password
-    if (!this.email || this.password) {
+    if (!this.email || !this.password) {
       alert('Provide email or password');
       return;
     }
     // if email and password is correct lets login the user
+    this.loginService.login(this.email, this.password);
+    // now we are logged in so we will redirect to our browse page
+    this.router.navigateByUrl('/browse');
   }
 }
