b6cd80ecd2af3044f8c4a6d0fdaaec4427092f50
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-06 19:39:32 +0100

    Header done

--- src/app/pages/browse/browse.component.ts
+++ src/app/pages/browse/browse.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HeaderComponent } from '../../components/header/header.component';
+
 @Component({
   selector: 'app-browse',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './browse.component.html',
   styleUrl: './browse.component.css',
 })
